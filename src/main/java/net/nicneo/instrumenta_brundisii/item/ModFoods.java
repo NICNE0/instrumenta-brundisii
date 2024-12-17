84ca632dfc1c1f219fa2a2bf4c53c1be96a072c5
NicNeo <89832921+NICNE0@users.noreply.github.com>
2024-12-17 08:01:32 -0500

    adding tomato, sweet_potato, lettuce, and opium

--- src/main/java/net/nicneo/instrumenta_brundisii/item/ModFoods.java
+++ src/main/java/net/nicneo/instrumenta_brundisii/item/ModFoods.java
@@ -7,5 +7,4 @@ import net.minecraft.world.food.FoodProperties;
 public class ModFoods {
     public static final FoodProperties CORN_COB = new FoodProperties.Builder().nutrition(4).saturationMod(0.3f).build();
 
-
 }
